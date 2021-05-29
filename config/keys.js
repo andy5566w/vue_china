cf0e2999a1ec9ca4268c5aa14bdc2a6ddfcc10ad
andy5566w <38370595+andy5566w@users.noreply.github.com>
2021-05-29 23:10:04 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURL: 'mongodb+srv://test:test123456@cluster0-21tso.mongodb.net/test?retryWrites=true&w=majority',
+    mongoURL: 'mongodb+srv://test:<passwrod>@cluster0-21tso.mongodb.net/test?retryWrites=true&w=majority',
     secretOfKey: 'secret'
 }
